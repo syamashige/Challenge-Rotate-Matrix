e143b57caa90c69cee277a837c710406aa81c748
Sarah <yamashige.sarah@gmail.com>
2018-07-15 21:34:12 -1000

    trying something different

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -37,21 +37,30 @@ module.exports = class MatrixRotator {
     //   newArr[0].push(this.matrix[i][0]);
     // }
     if (direction === "ClockWise") {
-      for (let i = this.matrix.length-1; i >= 0; i--) {
-        arrClockWise[0].push(this.matrix[i][0]);
-        arrClockWise[1].push(this.matrix[i][1]);
-        arrClockWise[2].push(this.matrix[i][2]);
-        arrClockWise[3].push(this.matrix[i][3]);
-        arrClockWise[4].push(this.matrix[i][4]);
-      } 
+
+      //Want to push into a new array and then push the array into the overall new array
+      for (let j = this.matrix.length -1; i >= 0; i--) {
+
+        for (let i = this.matrix.length-1; i >= 0; i--) {
+          arrClockWise[j].push(this.matrix[i][j]);
+          // arrClockWise[j].push(this.matrix[i][1]);
+          // arrClockWise[2].push(this.matrix[i][2]);
+          // arrClockWise[3].push(this.matrix[i][3]);
+          // arrClockWise[4].push(this.matrix[i][4]);
+        } 
+      }
+      
       this.matrix = arrClockWise;
     } else if (direction === "CounterClockWise") {
-      for (let i = 0; i < this.matrix.length; i++) {
-        arrCounterClockWise[0].push(this.matrix[i][4]);
-        arrCounterClockWise[1].push(this.matrix[i][3]);
-        arrCounterClockWise[2].push(this.matrix[i][2]);
-        arrCounterClockWise[3].push(this.matrix[i][1]);
-        arrCounterClockWise[4].push(this.matrix[i][0]);
+      for (let j = this.matrix.length -1; i >= 0; i--) {
+
+        for (let i = 0; i < this.matrix.length; i++) {
+          arrCounterClockWise[j].push(this.matrix[i][j]);
+          // arrCounterClockWise[1].push(this.matrix[i][3]);
+          // arrCounterClockWise[2].push(this.matrix[i][2]);
+          // arrCounterClockWise[3].push(this.matrix[i][1]);
+          // arrCounterClockWise[4].push(this.matrix[i][0]);
+        }
       }
       this.matrix = arrCounterClockWise;
     } else {
